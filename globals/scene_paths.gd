4568b29edf6e20790087947b6e0eeda68b62846c
Eilleen1202 <eilleen1202@gmail.com>
2025-02-09 15:14:37 -0500

    edit of ending scene path

--- globals/scene_paths.gd
+++ globals/scene_paths.gd
@@ -3,3 +3,5 @@ extends Node
 const player_interface_path: String = "res://ui/PlayerInterface.tscn"
 const game_interface_path: String = "res://ui/GameInterface.tscn"
 const hexagon_path: String = "res://scenes//hexagon.tscn"
+const ending_scene_path : String = "res://scenes//ending_display.tscn"
+const main_scene_path : String = "res://scenes/main.tscn"
